4ac1ed772cf568d122aea1eec364b9c82e77d058
Muhammad Zeeshan <mshan554252@gmail.com>
2026-07-08 23:18:38 +0500

    create form and chack input data and print seccfully.

--- load_html_form.js
+++ load_html_form.js
@@ -1,17 +1,35 @@
 const http = require("http")
 const fs = require("fs")
+const { buffer } = require("stream/consumers")
 
 http.createServer((req, resp)=>{
-    fs.readFile("html/form.html", "utf-8", (err, data)=>{
-        if(err){
-            resp.end("404 Page not found.")
-            return;
-        }
+    
         if(req.url == "/"){
-            resp.write(data)
+            fs.readFile("html/form.html", "utf-8", (err,data)=>{
+                if(err){
+                    resp.end("404 Page not found.")
+                    return;
+                }
+                resp.writeHead(200, {'content-type' : 'text/html'})
+                resp.write(data)
+                resp.end()
+            })
         }else if(req.url == "/submit"){
-            resp.write("<h1> Form Submitted. <h1>")
+            let formdata = [];
+            req.on('data', (chunk)=>{
+                formdata.push(chunk)
+            });
+
+            req.on('end', ()=>{
+                let rowdata = Buffer.concat(formdata).toString()
+                cleandata = new URLSearchParams(rowdata)
+
+                let name = cleandata.get('name')
+                let email = cleandata.get('email')
+                resp.write("<h1> Form Submitted. </h1>")
+                resp.write(`<h1>Name : ${name} </h1>`)
+                resp.write(`<h1>Email : ${email} </h1>`)
+                resp.end()
+            })
         }
-        resp.end()
-    })
 }).listen(4800)
