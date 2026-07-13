27fc3f1cbd4bf6076396098237c07bcf281e1956
Muhammad Zeeshan <mshan554252@gmail.com>
2026-07-13 16:24:20 +0500

    create delete and read file with command prompt input.

---
+++ fsWithCmd.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+
+const operation = process.argv[2]
+const name = process.argv[3]
+const content = process.argv[4]
+
+if (operation == "write"){
+    fs.writeFileSync(`text/${name}.txt`, content)
+}else if(operation == "read"){
+    data = fs.readFileSync(`text/${name}.txt`, "utf-8")
+    console.log(data)
+}else if(operation == "delete"){
+    fs.unlink(`text/${name}.txt`)
+}else {
+    console.log("Wrong input.")
+}
