cdd2793295c65bce199d59cc7706aec7c2ccd61a
Muhammad Zeeshan <mshan554252@gmail.com>
2026-07-23 22:00:36 +0500

    create a file with three html pages and link of each other.

---
+++ Express/render_html.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+
+app.get("", (req, resp)=>{
+    resp.send("<h1>Home Page.</h1><br><a href='/login'>Login</a>")
+    
+})
+app.get("/login", (req, resp)=>{
+    resp.send(`
+            <form action="/submit" method="post">
+                <input type="text" placeholder="name">
+                <input type="text" placeholder="name">
+                <button>submit</button>
+            </form>
+        `)
+})
+app.post("/submit", (req, resp)=>{
+    resp.send("<h1>Login Seccfully............</h1><br><a href='/'>Home Page</a>")
+})
+app.listen(4800)
